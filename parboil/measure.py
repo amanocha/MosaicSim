8cfea89faa1b3213eb36322b5afd5ef6ed494fb0
amanocha <amanocha@princeton.edu>
2019-12-20 04:26:40 -0500

    new graphs

--- parboil/measure.py
+++ parboil/measure.py
@@ -80,7 +80,7 @@ def compile(real):
 def execute(real):
     print("Executing application...")
     if app == "bfs":
-      datafiles = ["Kronecker_24.el"] #["graph_input.dat"]
+      datafiles = ["Kronecker_21.el"] #["graph_input.dat"]
     elif app == "cutcp":
       datafiles = ["watbox.sl40_mod.pqr"]
     elif app == "histo":
@@ -139,7 +139,7 @@ def simulate():
     out_files = [int(f.split("_")[2]) for f in files if "output_compute" in f]
     threads = max(out_files) + 1
 
-    cmd_args = ["pythiarun", "-n", str(threads), "."]
+    cmd_args = ["mosaicrun", "-n", str(threads), "."]
     cmd_sim = ["-sc", "sim_cafe", "-o", output]
     cmd_config = ["-cc", "core_cafe"]
     cmd = " ".join(cmd_args + cmd_sim + cmd_config)
